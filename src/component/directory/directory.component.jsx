43e9f4d5c0702aa995508d447f5f39d3d80d303f
kalpesh442266 <kalpeshbrane@gmaill.com>
2022-04-25 18:14:59 +0530

    first commit

---
+++ src/component/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import CategoryItem from '../category-item/categories.component';
+
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+//    console.log(categories)
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+          ))}
+    </div>
+  );
+};
+
+export default Directory;
